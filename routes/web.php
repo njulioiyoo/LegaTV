181dc50cd714bfeafedc6c8616b4d4b8fb5db4ff
Julio Notodiprodyo <julio.notodiprodyo@koltiva.com>
2023-04-20 05:49:23 +0700

    route home redirect to live

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect('live');
+    // return view('welcome');
 });
 
 Route::group(['prefix' => 'news'], function () {
