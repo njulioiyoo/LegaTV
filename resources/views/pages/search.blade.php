520779ab7fe7e8a48018d94ca4a699581223de75
Julio Notodiprodyo <julio.notodiprodyo@koltiva.com>
2023-04-26 20:39:37 +0700

    search empty

--- resources/views/pages/search.blade.php
+++ resources/views/pages/search.blade.php
@@ -7,20 +7,20 @@
 <div class="banner-wrap geeky-news">
     <!-- BANNER -->
     <div class="banner grid-limit">
-      <h2 class="banner-title">Search Results</h2>
-      <p class="banner-sections">
-        <span class="banner-section">Home</span>
-        <!-- ARROW ICON -->
-        <svg class="arrow-icon">
-          <use xlink:href="#svg-arrow"></use>
-        </svg>
-        <!-- /ARROW ICON -->
-        <span class="banner-section">{{ request()->input('key')   }}</span>
-      </p>
+        <h2 class="banner-title">Search Results</h2>
+        <p class="banner-sections">
+            <span class="banner-section">Home</span>
+            <!-- ARROW ICON -->
+            <svg class="arrow-icon">
+                <use xlink:href="#svg-arrow"></use>
+            </svg>
+            <!-- /ARROW ICON -->
+            <span class="banner-section">{{ request()->input('key')   }}</span>
+        </p>
     </div>
     <!-- /BANNER -->
-  </div>
-  <!-- /BANNER WRAP -->
+</div>
+<!-- /BANNER WRAP -->
 
 @include('templates.elements.live-news')
 
@@ -28,19 +28,19 @@
 <div class="layout-content-full grid-limit">
     <!-- GRID3 1COL -->
     <div class="grid3-1col">
-      <!-- WIDGET ITEM -->
-      <div class="widget-item">
-        <!-- FILTER HEADING -->
-        <div class="filter-heading no-border spaced">
-          <!-- FILTER HEADING TEXT -->
-          <p class="filter-heading-text red"><span class="highlight">{{ count($search) }}</span> Search Results for: "<span class="highlight">{{ request()->input('key') }}</span>"</p>
-          <!-- /FILTER HEADING TEXT -->
-        </div>
-        <!-- /FILTER HEADING -->
+        <!-- WIDGET ITEM -->
+        <div class="widget-item">
+            <!-- FILTER HEADING -->
+            <div class="filter-heading no-border spaced">
+                <!-- FILTER HEADING TEXT -->
+                <p class="filter-heading-text red"><span class="highlight">{{ count($search) }}</span> Search Results for: "<span class="highlight">{{ request()->input('key') }}</span>"</p>
+                <!-- /FILTER HEADING TEXT -->
+            </div>
+            <!-- /FILTER HEADING -->
 
-        <!-- ACTIVITY ITEMS -->
-        @foreach($search as $k => $v)
-        @php 
+            <!-- ACTIVITY ITEMS -->
+            @foreach($search as $k => $v)
+            @php
             $date = $v['created_at'];
             // Konversi string menjadi objek DateTime
             $dateObj = new DateTime($date);
@@ -51,65 +51,66 @@
 
             // Konversi selisih waktu ke dalam format "x waktu yang lalu"
             if ($diff->y > 0) {
-                $result = $diff->y . ' years ago';
+            $result = $diff->y . ' years ago';
             } elseif ($diff->m > 0) {
-                $result = $diff->m . ' months ago';
+            $result = $diff->m . ' months ago';
             } elseif ($diff->d > 0) {
-                $result = $diff->d . ' days ago';
+            $result = $diff->d . ' days ago';
             } elseif ($diff->h > 0) {
-                $result = $diff->h . ' hours ago';
+            $result = $diff->h . ' hours ago';
             } elseif ($diff->i > 0) {
-                $result = $diff->i . ' minutes ago';
+            $result = $diff->i . ' minutes ago';
             } else {
-                $result = 'just now';
+            $result = 'just now';
             }
-        @endphp
-        <div class="activity-items">
-          <!-- ACTIVITY ITEM -->
-          <div class="activity-item">
-            <!-- USER AVATAR -->
-            <img class="user-avatar" src="{{ $v['image'] }}" alt="user-05">
-            <!-- /USER AVATAR -->
+            @endphp
+            <div class="activity-items">
+                <!-- ACTIVITY ITEM -->
+                <div class="activity-item">
+                    <!-- USER AVATAR -->
+                    <img class="user-avatar" src="{{ $v['image'] }}" alt="user-05">
+                    <!-- /USER AVATAR -->
 
-            <!-- ACTIVITY ITEM DROPDOWN BUTTON -->
-            <div class="activity-item-dropdown-button">
-              <!-- ARROW ICON -->
-              <svg class="arrow-icon medium">
-                <use xlink:href="#svg-arrow-medium"></use>
-              </svg>
-              <!-- /ARROW ICON -->
+                    <!-- ACTIVITY ITEM DROPDOWN BUTTON -->
+                    <div class="activity-item-dropdown-button">
+                        <!-- ARROW ICON -->
+                        <svg class="arrow-icon medium">
+                            <use xlink:href="#svg-arrow-medium"></use>
+                        </svg>
+                        <!-- /ARROW ICON -->
+                    </div>
+                    <!-- /ACTIVITY ITEM DROPDOWN BUTTON -->
+
+                    <!-- ACTIVITY ITEM ROW -->
+                    <div class="activity-item-row">
+                        <!-- ACTIVITY ITEM TITLE -->
+                        <a href="{{ route(''.$v['type'].'.detail', $v['slug']) }}" class="activity-item-title">{{ $v['name'] }}</a>
+                        <!-- /ACTIVITY ITEM TITLE -->
+
+                        <!-- ACTIVITY ITEM TIMESTAMP -->
+                        <p class="activity-item-timestamp">{{ $result }}</p>
+                        <!-- /ACTIVITY ITEM TIMESTAMP -->
+                    </div>
+                    <!-- /ACTIVITY ITEM ROW -->
+
+                    <!-- FORUM CATEGORY TEXT -->
+                    <a href="#" class="forum-category-text cyan">{{ $v['parent']->name }}</a>
+                    <!-- /FORUM CATEGORY TEXT -->
+
+                    <!-- ACTIVITY ITEM TEXT -->
+                    <p class="activity-item-text">{!! Str::limit($v['body'], 200) !!}</p>
+                    <!-- /ACTIVITY ITEM TEXT -->
+                </div>
+                <!-- /ACTIVITY ITEM -->
             </div>
-            <!-- /ACTIVITY ITEM DROPDOWN BUTTON -->
-
-            <!-- ACTIVITY ITEM ROW -->
-            <div class="activity-item-row">
-              <!-- ACTIVITY ITEM TITLE -->
-              <a href="{{ route(''.$v['type'].'.detail', $v['slug']) }}" class="activity-item-title">{{ $v['name'] }}</a>
-              <!-- /ACTIVITY ITEM TITLE -->
-
-              <!-- ACTIVITY ITEM TIMESTAMP -->
-              <p class="activity-item-timestamp">{{ $result }}</p>
-              <!-- /ACTIVITY ITEM TIMESTAMP -->
-            </div>
-            <!-- /ACTIVITY ITEM ROW -->
-
-            <!-- FORUM CATEGORY TEXT -->
-            <a href="#" class="forum-category-text cyan">{{ $v['parent']->name }}</a>
-            <!-- /FORUM CATEGORY TEXT -->
-
-            <!-- ACTIVITY ITEM TEXT -->
-            <p class="activity-item-text">{!! Str::limit($v['body'], 200) !!}</p>
-            <!-- /ACTIVITY ITEM TEXT -->
-          </div>
-          <!-- /ACTIVITY ITEM -->
+            @endforeach
+            <!-- /ACTIVITY ITEMS -->
         </div>
-        @endforeach
-        <!-- /ACTIVITY ITEMS -->
-      </div>
-      <!-- /WIDGET ITEM -->
+        <!-- /WIDGET ITEM -->
 
-      <!-- PAGE NAVIGATION -->
-      <div class="page-navigation green center spaced">
+        <!-- PAGE NAVIGATION -->
+        @if(count($search) > 0)
+        <div class="page-navigation green center spaced">
             <!-- CONTROL PREVIOUS -->
             <div class="slider-control big control-previous">
                 <!-- ARROW ICON -->
@@ -145,9 +146,10 @@
             }
         </script>
         @endpush
+        @endif
     </div>
     <!-- GRID3 1COL -->
-  </div>
-  <!-- LAYOUT CONTENT FULL -->
+</div>
+<!-- LAYOUT CONTENT FULL -->
 
 @endsection
